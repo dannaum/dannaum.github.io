2e9d60058fa82d04e373a0dd0860c85a04de7766
Daniel Naumoski <daniel.naumoski.external@moonfare.com>
2022-06-02 10:04:36 +0200

    Update secondary-market.js

--- secondary-market.js
+++ secondary-market.js
@@ -2,40 +2,30 @@
 $(document).ready(function() {
 
     var auctionSlide = $('.secondary-auction_hover').outerWidth();
-    console.log(auctionSlide * 5);
+    var auctionSlideN = $('.secondary-auction-timeline_divider').find('.secondary-auction_hover').length;
     var screenWidth = $(window).width();
     var auctionTimelineSlide = $('.auction-timeline').outerWidth();
     var chartPrecision = $('.auction-timeline_drag-element').outerWidth();
     var auctionDrag = new Dragdealer('auction-timeline', {
     horizontal: true,
-    steps: 5,
+    steps: auctionSlideN,
     vertical: false,
     xPrecision: chartPrecision,
     callback: function(x, y) {
-        //auctionDrag getStep(x, y) in var
         var steps = auctionDrag.getStep() + '';
         var stepsArray = steps.split(',');
         var a = stepsArray[0];
         var b = stepsArray[1];
-        //for each callback
-        //auction-single-icon-wrap each
         $('.auction-single-icon-wrap').each(function(index) {
-            //this get index
             var thisIndex = $(this).index();
-            //if thisIndex equals or less than a
             if (thisIndex <= a) {
-                //this add class active-auction-icon
                 $(this).addClass('active-auction-icon');
-                //this find auction-timeline_icon-wrap
                 $(this).find('.auction-timeline_icon-wrap').addClass('auction-icon_active');
             }
             else
             {
-                //if thisIndex greater than a
                 if (thisIndex > a) {
-                    //this remove class active-auction-icon
                     $(this).removeClass('active-auction-icon');
-                    //this find auction-timeline_icon-wrap
                     $(this).find('.auction-timeline_icon-wrap').removeClass('auction-icon_active');
                 }
             }
@@ -45,7 +35,7 @@ $(document).ready(function() {
     animationCallback: function(x, y) {
         $('.auction-chart-timeline-line-active').css('width', Math.round(x * 100) + '%');
         $('.auction-timeline_drag-line-active').css('width', Math.round(x * 100) + '%');
-        $('.secondary-auction-timeline_divider').css('margin-left', -x * (5 * auctionSlide - screenWidth) );
+        $('.secondary-auction-timeline_divider').css('margin-left', -x * (auctionSlideN * auctionSlide - screenWidth) );
         $('.auction-timeline').css('margin-left', -x * (auctionTimelineSlide - screenWidth + 128));
     }
     });
