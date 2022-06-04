7e11cda92c8ad4dddabb3e4e45efa86bc8cb4775
Daniel Naumoski <daniel.naumoski.external@moonfare.com>
2022-06-04 12:45:53 +0200

    Update press.js

--- press.js
+++ press.js
@@ -1,26 +1,32 @@
 //on page load
 $(document).ready(function(){
-    //on click of knowledge-center_category-tag
+
     $('.knowledge-center_category-tag').on('click', function(){
-        $('.knowledge-center_category-tag').removeClass('active');
-        $(this).addClass('active');
+        $(this).siblings('.knowledge-center_category-tag').removeClass('active');
         var tagName = $(this).text().toLowerCase();
-        $('.press-search-grid').css('display', 'block');
-        $('.press-static-grid').css('display', 'none');
-        $('.press-search-grid').find('.single-press_item').each(function(){
-            var languageTag = $(this).find('.language-media-tag');
-            if($(languageTag).text().toLowerCase() == tagName){
-                $(this).css('display', 'block');
-            }
-            //else if tagName == 'all'
-            else if(tagName == 'all'){
-                $(this).css('display', 'block');
-                $('.press-search-grid').css('display', 'none');
-                $('.press-static-grid').css('display', 'block');
-            }
-            else {
-                $(this).css('display', 'none');
-            }
-        });
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $('.press-search-grid').css('display', 'none');
+            $('.press-static-grid').css('display', 'block');
+        }
+        else {
+            $(this).addClass('active');
+            $('.press-static-grid').css('display', 'none');
+            $('.press-search-grid').css('display', 'block');
+            $('.press-search-grid').find('.single-press_item').each(function(){
+                var languageTag = $(this).find('.language-media-tag');
+                if($(languageTag).text().toLowerCase() == tagName){
+                    $(this).css('display', 'block');
+                }
+                else if(tagName == 'all'){
+                    $(this).css('display', 'block');
+                    $('.press-search-grid').css('display', 'none');
+                    $('.press-static-grid').css('display', 'block');
+                }
+                else {
+                    $(this).css('display', 'none');
+                }
+            });
+        }
     });
 });
