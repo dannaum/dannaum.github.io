98482e76a6b760d21e79dab064b4a7c9c01859eb
Daniel Naumoski <daniel.naumoski.external@moonfare.com>
2022-06-04 12:34:39 +0200

    Update blog.js

--- blog.js
+++ blog.js
@@ -3,40 +3,30 @@ $(document).ready(function(){
     //on click of knowledge-center_category-tag html to lowercase in var
     $('.knowledge-center_category-tag').on('click', function(){
         var tag = $(this).text().toLowerCase();
-        //this add class active
-        //this siblings knowledge-center_category-tag remove class active
         $(this).siblings('.knowledge-center_category-tag').removeClass('active');
-        $(this).addClass('active');
-        //if this has class active
         if($(this).hasClass('active')){
-            //blog-hero_article-wrapper css display none
+            $(this).removeClass('active');
+            $('.blog-hero_article-wrapper').css('display', 'block');
+            $('.blog-articles_section').css('display', 'block');
+            $('.blog-articles_section-search').css('display', 'none');
+        }
+        else {
+            $(this).addClass('active');
             $('.blog-hero_article-wrapper').css('display', 'none');
-            //blog-articles_section css display none
             $('.blog-articles_section').css('display', 'none');
-            //blog-articles_section-search css display block
             $('.blog-articles_section-search').css('display', 'block');
-            //for each single-blog_item
             $('.single-blog_item').each(function(){
-                //this find hs6 html to lowercase in var
                 var tag2 = $(this).find('.single-media-tag').text().toLowerCase();
-                //if tag2 == tag
                 if(tag2 == tag){
-                    //this css display block
                     $(this).css('display', 'block');
                 }
-                //else
                 else{
-                    //this css display none
                     $(this).css('display', 'none');
                 }
-                //if tag2 == all
                 if(tag == 'all'){
-                    //this css display block
                     $(this).css('display', 'block');
                     $('.blog-hero_article-wrapper').css('display', 'block');
-                    //blog-articles_section css display none
                     $('.blog-articles_section').css('display', 'block');
-                    //blog-articles_section-search css display block
                     $('.blog-articles_section-search').css('display', 'none');
                 }
             });
