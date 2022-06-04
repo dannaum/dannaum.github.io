8bf6dd3b9dfa31e05c263decb018c363ad9ea8a2
Daniel Naumoski <daniel.naumoski.external@moonfare.com>
2022-06-04 10:39:33 +0200

    buyout growth

---
+++ buyout.js
@@ -0,0 +1,17 @@
+//on page load
+$(document).ready(function(){
+    var screenWidth = $(window).width();
+    var sp500Width = $('.direct-invest_graph').outerWidth();
+    var sp500slider = new Dragdealer('home-benefits-drag-tool', {
+        speed: 0.1,
+        requestAnimationFrame: true,
+        horizontal: true,
+        vertical: false,
+        xPrecision: sp500Width,
+        reflow: true,
+        animationCallback: function(x, y) {
+            $('.home-benefits_drag-line-active').css('width', Math.round(x * 100) + '%');
+            $('.direct-invest_graph-wrap').css('margin-left', -x * (sp500Width - screenWidth +64));
+        }
+    });
+});
