571a473347bbc083ed37178f7bd1224d0f96a094
Daniel Naumoski <daniel.naumoski.external@moonfare.com>
2022-06-08 09:06:02 +0200

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,7 @@
 //on page load
 $(document).ready(function(){
     
+    screenWidth = $(window).width();
     var sp500Width = $('.direct-invest_graph').outerWidth();
     var sp500slider = new Dragdealer('home-benefits-drag-tool', {
         speed: 0.1,
@@ -11,8 +12,13 @@ $(document).ready(function(){
         reflow: true,
         animationCallback: function(x, y) {
             $('.home-benefits_drag-line-active').css('width', Math.round(x * 100) + '%');
-            $('.direct-invest_graph-wrap').css('margin-left', -x * sp500Width / 16 + 'REM');
+            $('.direct-invest_graph-wrap').css('margin-left', -x * (sp500Width - screenWidth));
         }
     });
+
+    if(sp500Width > screenWidth){
+        $('.past-future-drag-component').css('display', 'block');
+        $('.home-benefits_drag-element').css('display', 'block');
+    }
 
 });
