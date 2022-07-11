a405e5b65c1061bdba4662424641f489eae885a9
Daniel Naumoski <daniel.naumoski.external@moonfare.com>
2022-07-11 16:20:14 +0200

    Update press.js

--- press.js
+++ press.js
@@ -1,26 +1,42 @@
 //on page load
 $(document).ready(function(){
+    //press-search-coverage-grid children ".single-pr-item" each
+    $('.press-search-coverage-grid .single-pr-item').each(function(){
+        if(index > 5){
+            $(this).hide();
+        }
+    });
+    $('#press-load-more').click(function(){
+        var lastIndex = $('.knowledge-center_item:visible').last().index();
+        console.log(lastIndex)
+        $('.knowledge-center_item').each(function(index){
+            if(index > lastIndex && index < lastIndex + 7){
+                $(this).show();
+            }
+        });
+    });
+    $('.knowledge-center_category-tag').eq(0).addClass('active');
     $('.knowledge-center_category-tag').on('click', function(){
         $(this).siblings('.knowledge-center_category-tag').removeClass('active');
         var tagName = $(this).text().toLowerCase();
         if($(this).hasClass('active')){
             $(this).removeClass('active');
-            $('.press-search-grid').css('display', 'none');
-            $('.press-static-grid').css('display', 'block');
+            $('.press-search-coverage-grid').css('display', 'none');
+            $('.press-static-coverage-grid').css('display', 'block');
         }
         else {
             $(this).addClass('active');
-            $('.press-static-grid').css('display', 'none');
-            $('.press-search-grid').css('display', 'block');
-            $('.press-search-grid').find('.single-press_item').each(function(){
-                var languageTag = $(this).find('.language-media-tag');
+            $('.press-static-coverage-grid').css('display', 'none');
+            $('.press-search-coverage-grid').css('display', 'block');
+            $('.press-search-coverage-grid').find('.single-pr-item').each(function(){
+                var languageTag = $(this).find('.single-media-tag');
                 if($(languageTag).text().toLowerCase() == tagName){
                     $(this).css('display', 'block');
                 }
                 else if(tagName == 'all'){
                     $(this).css('display', 'block');
-                    $('.press-search-grid').css('display', 'none');
-                    $('.press-static-grid').css('display', 'block');
+                    $('.press-search-coverage-grid').css('display', 'none');
+                    $('.press-static-coverage-grid').css('display', 'block');
                 }
                 else {
                     $(this).css('display', 'none');
