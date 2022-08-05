aaf31b231f8c89abe9feaf23cca5544f761cc3e7
Daniel Naumoski <daniel.naumoski.external@moonfare.com>
2022-08-05 17:52:57 +0200

    Update pe-101-articles.js

--- pe-101-articles.js
+++ pe-101-articles.js
@@ -8,8 +8,12 @@ $(document).ready(function() {
     
     $(".blog-rich-text").find("h2").each(function() {
         var text = $(this).text();
-        $('.glossary-article_navigation').append("<a class='glossary-article_navigation-link'><h3 class='hs7 bottom-margin-0'>" + text + "</h3></a>");
+        var textId = text.replace(/[^a-zA-Z0-9]/g, '');
+        textId = textId.toLowerCase();
+        $(this).attr("id", textId);
+        $('.glossary-article_navigation').append('<a href="#' + textId +'" class="glossary-article_navigation-link"><h3 class="hs7 bottom-margin-0">' + text + '</h3></a>');
     });
+
     $(".glossary-article_navigation-link").first().addClass("active");
 
     ($.fn.isInViewport = function () {
@@ -29,7 +33,7 @@ $(document).ready(function() {
             }
         });
     });
-    
+    /*
     $('.glossary-article_navigation-link').on('click', function() {
         var tabText = $(this).text();
         var navbarHeight = $('.navbar_section').outerHeight();
@@ -38,6 +42,7 @@ $(document).ready(function() {
             scrollTop: $('.blog-rich-text').find("h2").eq(whichSection).offset().top - (navbarHeight * 2)
         }, 1000);
     });
+    */
     if ($(window).width() > 991) {
         if ($(".glossary-article_navigation").children(".glossary-article_navigation-link").length > 0) {
             $(".glossary-article_navigation").css("display", "block");
@@ -63,6 +68,5 @@ $(document).ready(function() {
     script.type = 'application/ld+json';
     script.innerHTML = '{"@context":"http://schema.org","@type":["Article"],"headline":"TITLE","datePublished":"PUBLISHED","dateModified":"MODIFIED","image":"IMAGE ICON","description":"META DESCR","author":[{"@type":"Corporation","name":"Moonfare","@id":"https://corporation.moonfare.com","legalName":"Moonfare GmbH","description":"Moonfare is a technology platform that enables individuals and their advisors to invest in top-tier private equity funds, with minimum investments starting at €50,000.","url":"https://www.moonfare.com","logo":"https://assets-global.website-files.com/5fd0f5501ea5ad3d2b7f9c33/62348259e296cf998613d7c1_darkLogo1643704627890.png","foundingDate":"2016"}],"publisher":{"@type":"Corporation","name":"Moonfare","@id":"https://corporation.moonfare.com","legalName":"Moonfare GmbH","description":"Moonfare is a technology platform that enables individuals and their advisors to invest in top-tier private equity funds, with minimum investments starting at €50,000.","url":"https://www.moonfare.com","logo":"https://assets-global.website-files.com/5fd0f5501ea5ad3d2b7f9c33/62348259e296cf998613d7c1_darkLogo1643704627890.png","foundingDate":"2016"},"citation":["CITATIONS"],"mainEntityOfPage":{"@type":["WebPage"],"@id":"https://www.moonfare.com/pe-101/slug","breadcrumb":{"@type":"BreadcrumbList","itemListElement":[{"@type":"ListItem","position":1,"item":{"@id":"https://www.moonfare.com/pe-101","name":"PE101"}},{"@type":"ListItem","position":2,"item":{"@id":"https://www.moonfare.com/pe-101/slug","name":"TITLE"}}]}}}';
     document.body.appendChild(script);
-
 
 });
