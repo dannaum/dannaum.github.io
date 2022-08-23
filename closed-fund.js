b629dd098ade9bf01d639ac3b88d40ec225e651b
Daniel Naumoski <daniel.naumoski.external@moonfare.com>
2022-08-23 15:23:51 +0200

    format

--- closed-fund.js
+++ closed-fund.js
@@ -1,21 +1,18 @@
-//on page load
-$(document).ready(function () {
-    $('.fund-survey_content').find('.button-primary').css("pointer-events", "none");
+$('.fund-survey_content').find('.button-primary').css("pointer-events", "none");
 
-    $('.fund-survey_single-link').on('click', function () {
-        $(this).toggleClass('active');
+$('.fund-survey_single-link').on('click', function () {
+    $(this).toggleClass('active');
 
-        if ($('.fund-survey_single-link').hasClass('active')) {
-            $('.fund-survey_content').find('.button-primary').removeClass('disabled');
-            $('.fund-survey_content').find('.button-primary').css("pointer-events", "auto");
-        }
-        else {
-            $('.fund-survey_content').find('.button-primary').addClass('disabled');
-            $('.fund-survey_content').find('.button-primary').css("pointer-events", "none");
-        }
-    });
+    if ($('.fund-survey_single-link').hasClass('active')) {
+        $('.fund-survey_content').find('.button-primary').removeClass('disabled');
+        $('.fund-survey_content').find('.button-primary').css("pointer-events", "auto");
+    }
+    else {
+        $('.fund-survey_content').find('.button-primary').addClass('disabled');
+        $('.fund-survey_content').find('.button-primary').css("pointer-events", "none");
+    }
+});
 
-    $('.fund-survey_content').find('.button-primary').on('click', function () {
-        $('.fund-survey_popup').addClass('active');
-    });
+$('.fund-survey_content').find('.button-primary').on('click', function () {
+    $('.fund-survey_popup').addClass('active');
 });
